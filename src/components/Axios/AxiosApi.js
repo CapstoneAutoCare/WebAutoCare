a2e1f61047c5801cb389e9b9270de340f9d5f646
xuanDuy <xuanduy650@gmail.com>
2024-07-09 00:50:59 +0700

    fix port

--- src/components/Axios/AxiosApi.js
+++ src/components/Axios/AxiosApi.js
@@ -1,15 +1,17 @@
 import axios from "axios";
 
 const axiosApi = axios.create({
-    baseURL: "http://autocare.runasp.net/api",
-    Headers: { "Content-Type": "application/json" },
+  baseURL: "https://autocare.runasp.net/api",
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 axios.interceptors.response.use(
-    function(response) {
-        return response.data;
-    },
-    function(error) {
-        return Promise.reject(error);
-    }
+  function (response) {
+    return response.data;
+  },
+  function (error) {
+    return Promise.reject(error);
+  }
 );
 export default axiosApi;
